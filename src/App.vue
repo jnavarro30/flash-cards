607d2aa9281f8a81167a96ab44c029b186d74810
Jesse Navarro <jesvaro@macbook.local>
2026-05-12 23:39:37 -0700

    save

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Nav from './components/Nav.vue'
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <Nav />
+    </header>
+  </div>
+</template>
+
+<style scoped>
+header {
+  background-color: blue;
+  border: 0.2rem solid black;
+  width: 100%;
+  height: 5rem;
+  
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <button id="back-btn">Back</button>
+    <button id="sort-btn">Sort</button>
+    <button id="add-btn">Add</button>
+    <button id="edit-btn">Edit</button>
+    <!-- <button id="cancel-btn"> Cancel</button>
+    <button id="delete-btn">Delete</button> -->
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+button {
+  height: 3rem;
+  width: 5rem;
+  cursor: pointer;
+}
+</style>
